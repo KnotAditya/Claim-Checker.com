615e42d484ab770ae9469e214b5923d5afe7ed86
Aditya <122728491+KnotAditya@users.noreply.github.com>
2024-02-27 12:13:34 +0400

    database

---
+++ database.py
@@ -0,0 +1,21 @@
+#create fake database
+import backend
+
+#vidurs account details
+emriates_IDs = "12345678912345",
+Nationality = "Indian",
+age = "15",
+Full_name = "Vidur Arun"
+
+#user detialss
+emriate_ID = backend.id_number
+Nationa = backend.nationality
+dob = backend.date_of_birth
+Fullname = backend.full_name
+
+vidur_account = {
+    emriates_IDs,
+    Nationality,
+    age,
+    Full_name,
+}
